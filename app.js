b826d4f96a14b391deba3caeafe13e225c102a59
ilathem <isaiahlathem@gmail.com>
2025-04-30 11:31:27 -0500

    add catch all error middleware function

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+app.use((err, req, res, next) => {
+  // catch all error middleware function
+  console.error(err);
+  res.status(500).send(err);
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
