7c93be7895ef144eb11fae022d681bf6f979f529
ilathem <isaiahlathem@gmail.com>
2025-05-06 07:58:16 -0500

    implement bookRouter

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,11 +1,15 @@
+// import router to catch network call
 const { Router } = require("express");
+// import controller to pass execution
+const { getBookById } = require("../controllers/bookController");
 
+// create router
 const bookRouter = Router();
 
+// catch-all route for /books
 bookRouter.get("/", (req, res) => res.send("All books"));
-bookRouter.get("/:bookId", (req, res) => {
-  const { bookId } = req.params;
-  res.send(`Book ID: ${bookId}`);
-});
+// send /books/1 to the controller to handle data
+bookRouter.get("/:bookId", getBookById);
 
+// export to use in app
 module.exports = bookRouter;
