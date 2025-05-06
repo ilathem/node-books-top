8e0ff3724ce239f712647e22393e7211ed05182c
ilathem <isaiahlathem@gmail.com>
2025-05-06 07:54:35 -0500

    implement book controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,1 +1,17 @@
-// todo
+const db = require("../db");
+const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../error/CustomNotFoundError");
+
+const getBookById = asyncHandler(async (req, res) => {
+  const { bookId } = req.params; //route parameter extracted
+
+  const book = await db.getBookById(Number(bookId)); //db is accessed
+
+  if (!book) {
+    throw new CustomNotFoundError("Book not found!");
+  }
+
+  res.send(`Book name: ${book.name}`); // result is returned
+});
+
+module.exports = { getBookById };
