c51a016344921b1cbe7beab3cfb1328a3fe70b78
ilathem <isaiahlathem@gmail.com>
2025-05-06 08:13:21 -0500

    change bookRouter to use getAllBooks controller

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,13 +1,13 @@
 // import router to catch network call
 const { Router } = require("express");
 // import controller to pass execution
-const { getBookById } = require("../controllers/bookController");
+const { getBookById, getAllBooks } = require("../controllers/bookController");
 
 // create router
 const bookRouter = Router();
 
 // catch-all route for /books
-bookRouter.get("/", (req, res) => res.send("All books"));
+bookRouter.get("/", getAllBooks);
 // send /books/1 to the controller to handle data
 bookRouter.get("/:bookId", getBookById);
 
