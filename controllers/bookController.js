a4b45ed88d7945e5ce7d59234548246646cc21b6
ilathem <isaiahlathem@gmail.com>
2025-05-06 08:06:32 -0500

    implement bookController getAllBooks

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,6 +2,19 @@ const db = require("../db");
 const asyncHandler = require("express-async-handler");
 const CustomNotFoundError = require("../error/CustomNotFoundError");
 
+// for getting all book names and id's
+// async handler is like a try catch block I don't have to write
+const getAllBooks = asyncHandler(async (req, res) => {
+  // wait to get books from database
+  const books = await db.getAllBooks();
+
+  // handle error if no books in db
+  if (!books) throw new CustomNotFoundError("No books in the database!");
+
+  // send books back in response
+  res.send(`Books: ${books}`);
+});
+
 const getBookById = asyncHandler(async (req, res) => {
   const { bookId } = req.params; //route parameter extracted
 
