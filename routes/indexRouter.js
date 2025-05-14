52c72024dda8337bdd5b333a3e7c1618382e7754
ilathem <isaiahlathem@gmail.com>
2025-05-14 10:23:58 -0500

    implement ejs navbar

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,11 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
-indexRouter.get('/', (req, res) =>
-  res.render('index', { message: 'EJS rocks!' })
-);
+const links = [
+  { href: '/', text: 'Home' },
+  { href: 'about', text: 'About' },
+];
+
+indexRouter.get('/', (req, res) => res.render('index', { links: links }));
 
 module.exports = indexRouter;
